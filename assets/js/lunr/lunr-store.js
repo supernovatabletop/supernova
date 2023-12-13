6bd435f7b79373064068c93a7f190baa4c89b385
supernovatabletop <supernovatabletop@users.noreply.github.com>
2023-12-13 09:01:20 +0000

    deploy: dcfd4609b7099324bfbd25e367ffb1d5b46cd9e5

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,32 +5,8 @@ var store = [{
         "url": "/supernova/paarungen/phase1-paarung",
         "teaser": null
       },{
-        "title": "Phase2_paarung",
-        "excerpt":"Desmont musste leider pausieren und wird durch Paeu ersetzt. JustCoco666 hat die Liga leider verlassen.  ","categories": [],
-        "tags": [],
-        "url": "/supernova/paarungen/phase2-paarung",
-        "teaser": null
-      },{
-        "title": "Phase3_paarung",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/paarungen/phase3-paarung",
-        "teaser": null
-      },{
-        "title": "Phase4_paarung",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/paarungen/phase4-paarung",
-        "teaser": null
-      },{
-        "title": "Phase5_paarung",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/paarungen/phase5-paarung",
-        "teaser": null
-      },{
         "title": "Army",
-        "excerpt":"There is no truth in flesh, only betrayal. The Enemy is the Friend. The Friend was the Emperor. Only the Black Lords can rule. With these words of truth I lead my Army into battle. Cold calculations over emotions, I will rule my Army.  ","categories": [],
+        "excerpt":" ","categories": [],
         "tags": [],
         "url": "/supernova/spielerinnen/army",
         "teaser": null
@@ -41,46 +17,10 @@ var store = [{
         "url": "/supernova/spielerinnen/askonnox",
         "teaser": null
       },{
-        "title": "Autshorty",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/spielerinnen/autshorty",
-        "teaser": null
-      },{
         "title": "Bennz",
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "/supernova/spielerinnen/bennz",
-        "teaser": null
-      },{
-        "title": "Derjoker",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/spielerinnen/derjoker",
-        "teaser": null
-      },{
-        "title": "Desmont",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/spielerinnen/desmont",
-        "teaser": null
-      },{
-        "title": "Dubk1ng",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/spielerinnen/dubk1ng",
-        "teaser": null
-      },{
-        "title": "Grimm",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/spielerinnen/grimm",
-        "teaser": null
-      },{
-        "title": "Gripofchaos",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/spielerinnen/gripofchaos",
         "teaser": null
       },{
         "title": "Handsomejackiro",
@@ -102,15 +42,15 @@ var store = [{
         "teaser": null
       },{
         "title": "Keksbroesel",
-        "excerpt":"Felix aka “Keksbrösel” ist ein Spieler, der auf äußerst hinterhältige Methoden und psychologische Kriegsführung setzt. Nimm dich in Acht wenn du gegen seine Raven Guard, Spezialisten in Guerrilla Kämpfen, antrittst.  ","categories": [],
+        "excerpt":" ","categories": [],
         "tags": [],
         "url": "/supernova/spielerinnen/keksbroesel",
         "teaser": null
       },{
-        "title": "Luisderzwerg",
+        "title": "Linus",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/supernova/spielerinnen/luisderzwerg",
+        "url": "/supernova/spielerinnen/linus",
         "teaser": null
       },{
         "title": "Ma5ter19",
@@ -138,7 +78,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Rem",
-        "excerpt":"Christian aka “Rem”, Master of Ceremony und notorischer Prokastinator mit Hang zur Dramatik; will nur spielen.  ","categories": [],
+        "excerpt":" ","categories": [],
         "tags": [],
         "url": "/supernova/spielerinnen/rem",
         "teaser": null
@@ -159,18 +99,6 @@ var store = [{
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "/supernova/spielerinnen/scurvymcg",
-        "teaser": null
-      },{
-        "title": "Seebusch",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/spielerinnen/seebusch",
-        "teaser": null
-      },{
-        "title": "Sonnenbursche",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/spielerinnen/sonnenbursche",
         "teaser": null
       },{
         "title": "Stardewd",
@@ -195,29 +123,5 @@ var store = [{
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "/supernova/ranglisten/phase1-rangliste",
-        "teaser": null
-      },{
-        "title": "Phase2_rangliste",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/ranglisten/phase2-rangliste",
-        "teaser": null
-      },{
-        "title": "Phase3_rangliste",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/ranglisten/phase3-rangliste",
-        "teaser": null
-      },{
-        "title": "Phase4_rangliste",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/ranglisten/phase4-rangliste",
-        "teaser": null
-      },{
-        "title": "Phase5_rangliste",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/supernova/ranglisten/phase5-rangliste",
         "teaser": null
       }]
