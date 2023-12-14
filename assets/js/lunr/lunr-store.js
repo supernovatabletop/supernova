be2ab06a502ebcf87b1ec54dd944c75decb2844c
supernovatabletop <supernovatabletop@users.noreply.github.com>
2023-12-14 21:59:37 +0000

    deploy: 0405fe8ccb5f43f5433618089f5ecd15eb150ba5

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -83,6 +83,12 @@ var store = [{
         "url": "/supernova/spielerinnen/linus",
         "teaser": null
       },{
+        "title": "Lionel",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/supernova/spielerinnen/lionel",
+        "teaser": null
+      },{
         "title": "Lupublic",
         "excerpt":" ","categories": [],
         "tags": [],
